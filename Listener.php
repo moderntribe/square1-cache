6d12475a05e25d59dd1928f4dabe91d2b47ca23d
Tr1b0t <Tr1b0t@users.noreply.github.com>
2022-08-21 20:22:24 +0000

    prepare release

---
+++ Listener.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tribe\Libs\Cache;
+
+/**
+ * Tracks WP events and expires select caches as necessary
+ */
+class Listener {
+	/** @var Cache */
+	protected $cache;
+
+	public function __construct( Cache $cache = null ) {
+		$this->cache = $cache ?: new Cache();
+	}
+}
